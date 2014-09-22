33d5645d8b3576c6210b5da155288dc1bee63721
Magnus Skjegstad <magnus@skjegstad.com>
2014-09-22 10:20:47 +0100

    Added jitsu test

---
+++ test-jitsu/local/query_and_connect.py
@@ -0,0 +1,28 @@
+# code from http://stackoverflow.com/questions/3898363/python-dns-resolver-set-specific-dns-server
+import dns.resolver,sys
+import socket
+
+resolver_ip = sys.argv[1]
+print "Using",resolver_ip," as DNS server"
+
+hostname = sys.argv[2]
+print "Resolving hostname",hostname
+
+port = 80
+
+my_resolver = dns.resolver.Resolver()
+
+# 8.8.8.8 is Google's public DNS server
+my_resolver.nameservers = [resolver_ip]
+
+answer = my_resolver.query(hostname)
+for rdata in answer:
+    ip = rdata.to_text() # just use first answer returned
+    break
+
+sock = socket.socket()
+sock.connect((ip, port))
+sock.close()
+
+print "Connect/disconnect to ip",ip,"port",port
+
