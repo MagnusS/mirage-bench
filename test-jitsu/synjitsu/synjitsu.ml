34cfb497ada1749f706799cdfa72d40a72d5b611
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-09-25 13:35:53 +0100

    Flush the console regularly in synjitsu

--- test-jitsu/synjitsu/synjitsu.ml
+++ test-jitsu/synjitsu/synjitsu.ml
@@ -6,12 +6,21 @@ module Main (C: CONSOLE) (S: STACKV4) = struct
 
   module I = Init
 
+  let rec loop c =
+    C.log_s c "I'm alive!" >>= fun () ->
+    OS.Time.sleep 1. >>= fun () ->
+    loop c
+
   let start c s =
     I.start ();
     Tcpv4.Pcb.set_mode `Fast_start_proxy;
 
     (* listen on all ports *)
     S.listen_tcpv4 s (-1) (fun _ -> return_unit);
-    S.listen s
+
+    Lwt.join [
+      loop c;
+      S.listen s
+    ]
 
 end
